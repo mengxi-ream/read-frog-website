08d2f1c29b185af8180c8d5543b3f0759e36d670
Kuiliang Zhang <z1219202167@outlook.com>
2025-05-07 14:57:35 -0700

    fix: type of params

--- src/app/[locale]/(home)/page.tsx
+++ src/app/[locale]/(home)/page.tsx
@@ -1,15 +1,23 @@
 import { Header } from "@/components/hero/header";
 import { Demo } from "@/components/hero/demo";
+import { use } from "react";
 import { setRequestLocale } from "next-intl/server";
+import { Locale } from "@/i18n/routing";
 
-export default function HomePage({ params }: { params: { locale: string } }) {
-  setRequestLocale(params.locale);
+export default function HomePage({
+  params,
+}: {
+  params: Promise<{ locale: Locale }>;
+}) {
+  const { locale } = use(params);
+  setRequestLocale(locale);
   // const t = useTranslations("home");
 
   return (
     <main className="flex flex-1 flex-col dark:bg-[#18181b]">
       <Header />
       <Demo />
+      {/* <p>{t("title")}</p> */}
     </main>
   );
 }
