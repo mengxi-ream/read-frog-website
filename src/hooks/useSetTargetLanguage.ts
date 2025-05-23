0abc9846e6e0657de83866abebd9bc2fca5b339c
Kuiliang Zhang <z1219202167@outlook.com>
2025-05-22 19:52:25 -0700

    feat: message with extension

--- src/hooks/useSetTargetLanguage.ts
+++ src/hooks/useSetTargetLanguage.ts
@@ -1,15 +1,33 @@
 "use client";
 
+import { LangCodeISO6393, LOCALE_TO_ISO6393 } from "@/types/languages";
+import { useLocale } from "next-intl";
 import { useEffect, useState } from "react";
 
 export function useExtensionTargetLanguage() {
-  const [targetLanguage, setTargetLanguage] = useState<string | null>(null);
+  const [targetLanguage, setTargetLanguage] = useState<LangCodeISO6393 | null>(
+    null
+  );
+  const locale = useLocale();
 
   useEffect(() => {
     // get target language from the web page
-    const targetLanguage = document.documentElement.lang ?? "en";
-    setTargetLanguage(targetLanguage);
+    const websiteLanguage = LOCALE_TO_ISO6393[locale] ?? "eng";
+    updateTargetLanguage(websiteLanguage);
   }, []);
+
+  const updateTargetLanguage = (langCodeISO6393: LangCodeISO6393) => {
+    // send message to extension to set target language
+    window.postMessage(
+      {
+        source: "read-frog-page",
+        type: "setTargetLanguage",
+        langCodeISO6393: langCodeISO6393,
+      },
+      "*"
+    );
+    setTargetLanguage(langCodeISO6393);
+  };
 
   return targetLanguage;
 }
